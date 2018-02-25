cdfba6c39e6f9abf6d85894c3155f36d66b332eb
Domenic Garreffa <garreffd@uwindsor.ca>
2018-02-25 18:19:14 -0500

    refactor: Removed xml configuration and in place added Java configuration

---
+++ src/main/java/com/garreffd/config/AppConfig.java
@@ -0,0 +1,93 @@
+package com.garreffd.config;
+
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+
+import org.hibernate.SessionFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages="com.garreffd")
+@PropertySource("classpath:persistance-mysql.properties")
+public class AppConfig {
+	
+	
+	@Autowired
+	private Environment env;
+	//define a bean for the ViewResolver - The view resolver will aid in mapping the .jsp pages returned from the controller
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+				
+		return viewResolver;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan(env.getProperty("sessionFactory.packagesToScan"));
+		
+		return sessionFactory;
+	}
+	
+	
+	@Bean
+	public DataSource dataSource() {
+		//create connection pool
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		
+		//set jdbc driver class
+		try {
+			dataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//set database connection properties
+		dataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		dataSource.setUser(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		//Values read from the properties file are returned as a string.
+		//set connection pool properties
+		dataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+		dataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+		dataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+		dataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+		
+		return dataSource;
+	}
+	
+	@Bean
+	@Autowired
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		
+		return transactionManager;
+	}
+}
